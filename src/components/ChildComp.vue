72c4e8439b08eac1d5b92adb951d070dcd3a9542
Milan Belal <mohitbelal9@gmail.com>
2025-03-28 10:15:33 +0545

    css binding

--- src/components/ChildComp.vue
+++ src/components/ChildComp.vue
@@ -3,6 +3,9 @@
 <h3>{{ radhe }}</h3>
 <h2 v-for="item in users" :key="item.email">{{ item.name }}</h2>
 <button v-on:click="parentFunction()">Click to call parent function</button>
+
+<h2 :class="applyStyle">class Binding</h2>
+<button v-on:click="colorful=!colorful">change style</button>
 </template>
 <script>
 export default{
@@ -11,6 +14,34 @@ export default{
         radhe:String,
         users:Object,
         parentFunction:Function
+    },
+    data(){
+        return{
+            colorful:false
+        }
+    },
+    computed:{
+        applyStyle(){
+            return{
+                green:this.colorful,
+                err:true
+
+
+            }
+        }
+
     }
 }
 </script>
+
+<style>
+.green{
+    background-color: green;
+    width: auto;
+    justify-content: center;
+
+}
+.err{
+    color:red;
+}
+</style>
